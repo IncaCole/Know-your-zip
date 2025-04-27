ff38a57100522838c1c1277bfb9e1abb16506d26
Sal <sally.posey001@mymdc.net>
2025-04-26 22:21:45 -0400

    Add back plotly.express import for tab charts

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import plotly.express as px
 from education import EducationAPI
 from healthcare import HealthcareAPI
 from emergency_services import EmergencyServicesAPI
